950d3d5b5133a2560716838f6f1ad49b9e270b34
arysuri <arysuri23@gmail.com>
2022-10-30 13:12:27 -0400

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,1 +1,80 @@
+import pandas as pd
+import requests, json, csv
+from sqlalchemy import create_engine
+from io import StringIO
+
+### DOCUMENTATION ###
+### This file will first fetch data from the api when ran, then prompt the user to indicate what form they would like the data to be
+### transformed to(CSV or SQL), then it will create a new column, then print out a summary of the data.
+
+
+def jsonToCSV(data):
+    df = pd.read_json(data.text, orient='records')
+
+    return df.to_csv()
+
+def jsonToSQL(data):
+
+    df = pd.read_json(data.text, orient='records')
+
+    engine = create_engine('sqlite://', echo=False)
+    df.to_sql('BeerStyles', con=engine)
+    return engine.execute("SELECT * FROM BeerStyles").fetchall()
+    
+
+print("Benchmark i.1: Beer Data is fetched from api")
+#Source: https://rustybeer.herokuapp.com/docs#/default/styles
+
+beerStylesJSON = requests.get('https://rustybeer.herokuapp.com/styles')
+print("Initial json data: ")
+print(beerStylesJSON.json())
+print("\n")
+
+
+print("Benchmark i.2 change output form based on user input")
+
+changedForm = input("The beer data is currently in json form. Enter the format(CSV or SQL) you would like to convert it to: ")
+
+while changedForm != "CSV" and changedForm != "SQL":
+    changedForm = input("Please enter either CSV or SQL: ")
+
+if changedForm == "CSV":
+    beerStylesCSV = jsonToCSV(beerStylesJSON)
+    print("New data: ")
+    print(beerStylesCSV)
+
+elif changedForm == "SQL":
+    beerStylesSQL = jsonToSQL(beerStylesJSON)
+    print("New data: ")
+    print(beerStylesSQL)
+
+
+print("\n")
+
+print("Benchmark i.3 Modify columns, add column for range of abv")
+
+
+df = pd.read_json(beerStylesJSON.text, orient='record')
+df['abv_range'] = df.apply(lambda row: row.abv_max - row.abv_min, axis = 1)
+print(df['abv_range'])
+
+print("\n")
+
+print("Benchmark i.5 Brief Summary: ")
+
+print("Number of records: " + str(len(df)))
+
+print("Number of columns: " + str(len(df.columns)))
+
+
+
+
+
+
+
+
+
+
+
+
 
